cbd2e96e2ecd33dfd828e9dfa69c97a096ae2377
jiangpeipei <jiangpeipei@qutoutiao.net>
2019-09-10 17:09:55 +0800

    location

--- src/main/scala/org/interestinglab/waterdrop/filter/SubTableSql.scala
+++ src/main/scala/org/interestinglab/waterdrop/filter/SubTableSql.scala
@@ -90,17 +90,33 @@ class SubTableSql extends BaseFilter {
         case 1 => {
           if (ds.collect()(0).schema.fields(0).dataType.isInstanceOf[IntegerType]) {
             val a = ds.collect()(0).getAs[Int](0)
-            val b = ds.collect()(0).getAs[Int](1)
-            lower = java.lang.Long.parseLong(String.valueOf(a))
-            upper = java.lang.Long.parseLong(String.valueOf(b))
+            if (a == null || a.equals("null")) {
+              lower=0l
+              upper=0l
+            }else {
+              val b = ds.collect()(0).getAs[Int](1)
+              lower = java.lang.Long.parseLong(String.valueOf(a))
+              upper = java.lang.Long.parseLong(String.valueOf(b))
+            }
           } else if (ds.collect()(0).schema.fields(0).dataType.isInstanceOf[DecimalType]) {
             val a = ds.collect()(0).getAs[java.math.BigDecimal](0)
-            val b = ds.collect()(0).getAs[java.math.BigDecimal](1)
-            lower = java.lang.Long.parseLong(String.valueOf(a))
-            upper = java.lang.Long.parseLong(String.valueOf(b))
+            if (a == null || a.equals("null")) {
+              lower=0l
+              upper=0l
+            }else {
+              val b = ds.collect()(0).getAs[java.math.BigDecimal](1)
+              lower = java.lang.Long.parseLong(String.valueOf(a))
+              upper = java.lang.Long.parseLong(String.valueOf(b))
+            }
           } else {
-            lower = ds.collect()(0).getAs[Long](0)
-            upper = ds.collect()(0).getLong(1)
+            val a= ds.collect()(0).getAs[Long](0);
+            if (a == null || a.equals("null")) {
+              lower=0l
+              upper=0l
+            }else {
+              lower = ds.collect()(0).getAs[Long](0)
+              upper = ds.collect()(0).getLong(1)
+            }
           }
           (lower, upper)
         }
@@ -178,8 +194,8 @@ class SubTableSql extends BaseFilter {
         sb.append("PARTITIONED BY ( shard string)")
       }
       sb.append("stored as parquet ")
-      if(this.conf.hasPath("location")){
-        if(this.conf.getString("location")!=null || !this.conf.getString("location").equals("")){
+      if (this.conf.hasPath("location")) {
+        if (this.conf.getString("location") != null || !this.conf.getString("location").equals("")) {
           val location = this.conf.getString("location")
           sb.append(s" location '$location'")
         }
