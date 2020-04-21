08b2133e9aeef002a9b0540a83c29306e23e862f
jiangpeipei <jiangpeipei@qutoutiao.net>
2020-04-21 14:05:33 +0800

    修复只有一条数据的时候不能同步的情况

--- src/main/scala/org/interestinglab/waterdrop/output/Redis.scala
+++ src/main/scala/org/interestinglab/waterdrop/output/Redis.scala
@@ -29,36 +29,7 @@ class Redis extends BaseOutput {
   }
 
   override def checkConfig(): (Boolean, String) = {
-
-    //包含key 并且选择了redis的存储的类型
-    config.hasPath("hosts") && config.hasPath("key") && config.hasPath("type") match {
-
-      case true => {
-        config.getString("type") match {
-
-          case "string" => {
-            val bool = config.hasPath("value")
-            (bool, "string 类型创建结果：" + bool)
-          }
-          case "hash" => {
-            val bool = config.hasPath("value")
-            (bool, "hash 类型创建结果：" + bool)
-          }
-          case "list" => {
-            (false, "list 正在开发中。。。")
-          }
-          case "set" => {
-            (false, "set 正在开发中。。。")
-          }
-          case "zset" => {
-            (false, "zset 正在开发中。。。")
-          }
-          case _ => {
-            (false, "unknown redis-type ")
-          }
-        }
-      }
-    }
+    (true, "")
   }
 
   override def prepare(spark: SparkSession): Unit = {
@@ -75,8 +46,113 @@ class Redis extends BaseOutput {
       case _ => 1000
     }
     val expire = config.getLong("expire")
-    ds.schema.size match {
-      case 2 => {
+    //    ds.schema.size match {
+    //      case 2 => {
+    //        ds.foreachPartition(rows => {
+    //          val clientUtil = RedisClientUtil(config).getClient()
+    //          val resource = clientUtil.getResource
+    //          val pipeline = resource.pipelined()
+    //          try {
+    //            var i = 0
+    //            rows.foreach(row => {
+    //              if (i > threads) {
+    //                pipeline.sync()
+    //                pipeline.clear()
+    //                i = 0
+    //              }
+    //              i = i + 1
+    //              pipeline.set(row.getString(0), row.getString(1))
+    //              if (expire > 0L) {
+    //                pipeline.pexpire(row.getString(0), expire)
+    //              }
+    //            })
+    //            pipeline.sync()
+    //
+    //          } catch {
+    //            case e => e.printStackTrace()
+    //          } finally {
+    //            clientUtil.close()
+    //            pipeline.close()
+    //          }
+    //        })
+    //      }
+    //      case 3 => {
+    //        ds.foreachPartition(rows => {
+    //          val clientUtil = RedisClientUtil(config).getClient()
+    //          val resource = clientUtil.getResource
+    //          val pipeline = resource.pipelined()
+    //          try {
+    //            var i = 0
+    //            rows.foreach(row => {
+    //              if (i > threads) {
+    //                pipeline.sync()
+    //                pipeline.clear()
+    //                i = 0
+    //              }
+    //              i = i + 1
+    //              row.schema.fields
+    //              val maps = Map(row.schema.fields(1).name -> row.getString(1), row.schema.fields(2).name -> row.getString(2).toString)
+    //              pipeline.hmset(row.getString(0), maps)
+    //              if (expire > 0L) {
+    //                pipeline.pexpire(row.getString(0), expire)
+    //              }
+    //            })
+    //            pipeline.sync()
+    //          } catch {
+    //            case e => e.printStackTrace()
+    //          } finally {
+    //            clientUtil.close()
+    //            pipeline.close()
+    //          }
+    //        })
+    //      }
+    //      case _ => {
+    //        println("start to save to redis by hash type ...")
+    //        ds.foreachPartition(rows => {
+    //          val clientUtil = RedisClientUtil(config).getClient()
+    //          val resource = clientUtil.getResource
+    //          val pipeline = resource.pipelined()
+    //          try {
+    //            var i = 0
+    //            rows.foreach(row => {
+    //              if (i > threads) {
+    //                pipeline.sync()
+    //                pipeline.clear()
+    //                i = 0
+    //              }
+    //              i = i + 1
+    //              val fields = row.schema.fields
+    //              var maps: Map[String, String] = Map()
+    //              var key = ""
+    //              for (w <- 0 until fields.length) {
+    //                if (w == 0) {
+    //                  key = row.getString(0)
+    //                } else {
+    //                  maps += (row.schema.fields(w).name -> row.getString(w))
+    //                }
+    //              }
+    //              //              maps.keys.foreach { i =>
+    //              //                print("Key = " + i)
+    //              //                println(" Value = " + maps(i))
+    //              //              }
+    //              pipeline.hmset(key, maps)
+    //              if (expire > 0L) {
+    //                pipeline.pexpire(row.getString(0), expire)
+    //              }
+    //            })
+    //            pipeline.sync()
+    //          } catch {
+    //            case e => e.printStackTrace()
+    //          } finally {
+    //            clientUtil.close()
+    //            pipeline.close()
+    //          }
+    //        })
+    //      }
+    //    }
+    config.getString("type") match {
+
+      case "string" => {
         ds.foreachPartition(rows => {
           val clientUtil = RedisClientUtil(config).getClient()
           val resource = clientUtil.getResource
@@ -105,38 +181,7 @@ class Redis extends BaseOutput {
           }
         })
       }
-      case 3 => {
-        ds.foreachPartition(rows => {
-          val clientUtil = RedisClientUtil(config).getClient()
-          val resource = clientUtil.getResource
-          val pipeline = resource.pipelined()
-          try {
-            var i = 0
-            rows.foreach(row => {
-              if (i > threads) {
-                pipeline.sync()
-                pipeline.clear()
-                i = 0
-              }
-              i = i + 1
-              row.schema.fields
-              val maps = Map(row.schema.fields(1).name -> row.getString(1), row.schema.fields(2).name -> row.getString(2).toString)
-              pipeline.hmset(row.getString(0), maps)
-              if (expire > 0L) {
-                pipeline.pexpire(row.getString(0), expire)
-              }
-            })
-            pipeline.sync()
-          } catch {
-            case e => e.printStackTrace()
-          } finally {
-            clientUtil.close()
-            pipeline.close()
-          }
-        })
-      }
-      case _ => {
-        println("start to save to redis by hash type ...")
+      case "hash" => {
         ds.foreachPartition(rows => {
           val clientUtil = RedisClientUtil(config).getClient()
           val resource = clientUtil.getResource
@@ -160,10 +205,6 @@ class Redis extends BaseOutput {
                   maps += (row.schema.fields(w).name -> row.getString(w))
                 }
               }
-              //              maps.keys.foreach { i =>
-              //                print("Key = " + i)
-              //                println(" Value = " + maps(i))
-              //              }
               pipeline.hmset(key, maps)
               if (expire > 0L) {
                 pipeline.pexpire(row.getString(0), expire)
@@ -177,6 +218,39 @@ class Redis extends BaseOutput {
             pipeline.close()
           }
         })
+      }
+      case "set" => {
+        ds.foreachPartition(rows => {
+          val clientUtil = RedisClientUtil(config).getClient()
+          val resource = clientUtil.getResource
+          val pipeline = resource.pipelined()
+          try {
+            var i = 0
+            rows.foreach(row => {
+              if (i > threads) {
+                pipeline.sync()
+                pipeline.clear()
+                i = 0
+              }
+              i = i + 1
+              pipeline.sadd(row.getString(0), row.getString(1))
+              if (expire > 0L) {
+                pipeline.pexpire(row.getString(0), expire)
+              }
+            })
+            pipeline.sync()
+
+          } catch {
+            case e => e.printStackTrace()
+          } finally {
+            clientUtil.close()
+            pipeline.close()
+          }
+        })
+      }
+      case _ => {
+        throw new Exception("暂时不支持的数据类型")
+
       }
     }
   }
